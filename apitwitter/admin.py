f04034d5a8ea3c23712c23c7c3fee26f33822f90
Luan Vinícius <luanviniciuzz@gmail.com>
2024-10-23 12:45:39 -0300

    refacator: deleting files for refactoring

--- apitwitter/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from apitwitter.models import Tweet, TweetLike, TweetRetweet
-
-admin.site.register(Tweet)
-admin.site.register(TweetLike)
-admin.site.register(TweetRetweet)
