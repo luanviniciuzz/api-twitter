0b08186c46f99648b01f5fd307a3369d77887611
Luan Vinícius <luanviniciuzz@gmail.com>
2024-10-27 17:25:27 -0300

    feat: setup postgresql database

--- setup/settings.py
+++ setup/settings.py
@@ -79,12 +79,22 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql',
+          'NAME': 'TwitterDB',
+          'USER': 'postgres',
+          'PASSWORD': 'postgreSQL1z2x3c',
+          'HOST': 'localhost',
+          'PORT': '5432',
+     }
+  }
 
 
 # Password validation
